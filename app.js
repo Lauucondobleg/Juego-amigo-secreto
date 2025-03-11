46ec10b2a926a6ff9b0081631caacd9fadc38943
Lauucondobleg <laurayuribito19@gmail.com>
2025-03-11 15:42:18 -0500

    cargar archivos
    se suben archivos con lo trabajado hasta el momento

---
+++ app.js
@@ -0,0 +1,37 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/*
+let nombres = [];
+let inputAmigo = document.getElementById("amigo");
+let listaAmigo = document.getElementById("listaAmigo");
+let dResultado = document.getElementById("resultado");
+
+function agregarAmigo(){
+    if(!inputAmigo.value){
+        alert("Ingresa un nombre");
+    }else {
+        nombres.push(inputAmigo.value);
+        listaAmigo.innerHTML += `<li>${inputAmigo.value}</li>`;
+        limpiarCaja();
+    }
+    
+    console.log(nombres);
+}
+
+function sorteoAmigo(){
+    if (nombres.length ==0) {
+        alert("Debes agregar al menos un nombre antes de sortear.");
+        return;
+    }
+}
+
+    let random = Math.floor(Math.random()*nombres.length);
+    let amigoSecreto = nombres[random];
+    Resultado.innerHTML = `<h2> tu amigo secreto es ${amigoSecreto}</h2>`
+
+function limpiarCaja(){
+    document.querySelector("#amigo").value = " ";
+}
+*/
+
+
+let 
